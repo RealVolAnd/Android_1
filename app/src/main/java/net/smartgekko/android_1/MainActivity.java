68c882db948954d94e3da7ec219aa569b8452034
RealVolAnd <out_of_bound@yahoo.com>
2020-12-20 15:39:25 +0300

    Homework_1. Заготовка для погоды. Добавлена локализация RU.

--- app/src/main/java/net/smartgekko/android_1/MainActivity.java
+++ app/src/main/java/net/smartgekko/android_1/MainActivity.java
@@ -5,16 +5,15 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
-
 import android.os.Bundle;
-
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] titles = new String[]{"24 HOURS", "3 DAYS", "2 WEEKS"};
+
     private FragmentStateAdapter pagerAdapter;
     ViewPager2 viewPager2;
+    private String[] titles;
     private static final int NUM_PAGES = 3;
 
     @Override
@@ -22,7 +21,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //======================================
+        String f1=getString(R.string.HOURS_24);
+        String f2=getString(R.string.DAYS_3);
+        String f3=getString(R.string.WEEKS_2);
 
+        titles = new String[]{f1, f2, f3};
 
         TabLayout tabLayout =( TabLayout) findViewById(R.id.tabLayout);
         viewPager2 =( ViewPager2) findViewById(R.id.viewPager2);
@@ -44,17 +47,17 @@ public class MainActivity extends AppCompatActivity {
         public Fragment createFragment(int pos) {
             switch (pos) {
                 case 0: {
-                    return TodayFragment.newInstance("fragment 1");
+                    return TodayFragment.newInstance("fp1");
                 }
                 case 1: {
 
-                    return ThreeDaysFragment.newInstance("fragment 2");
+                    return ThreeDaysFragment.newInstance("fp2");
                 }
                 case 2: {
-                    return OneWeekFragment.newInstance("fragment 3");
+                    return OneWeekFragment.newInstance("fp3");
                 }
                 default:
-                    return TodayFragment.newInstance("fragment 1, Default");
+                    return TodayFragment.newInstance("fp1, Default");
             }
         }
 
